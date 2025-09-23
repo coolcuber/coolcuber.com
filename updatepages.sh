7de21bdf1e9da35159ad85f4aab745713972bde9
coolcuber <coolcuber@vtluug.org>
2025-09-23 14:20:20 -0400

    Changed update script

--- updatepages.sh
+++ updatepages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-set -euo pipefail
+set -feuo pipefail
 
 # page source directory
 PAGE_FILE_DIR=page_files/
@@ -10,11 +10,28 @@ PAGE_DIR=pages/
 cd "$(dirname $0)"
 
 # check for directory
-[ -e "$PAGE_DIR" ] && rm -r "$PAGE_DIR"* || mkdir "$PAGE_DIR"
+[ -e "$PAGE_DIR" ] || mkdir "$PAGE_DIR"
 
-# replace date on each page
+# generate pages that need to be generated
 datestr="<em>This page was last updated at $(date +"%H:%M") on $(date +"%m\/%d\/%Y").<\/em>"
-cd "$PAGE_FILE_DIR"
-for page in *; do
-	sed "s/<!--DATE-->/$datestr/g" "$page" > "../$PAGE_DIR$page"
+for page in $(find "$PAGE_FILE_DIR" -type f -printf "%P\n"); do
+	[ -e "$PAGE_DIR$page" ] && continue
+	page_dir=$(dirname "$page")
+	[ -d "$PAGE_DIR$page_dir" ] || { mkdir -p "$PAGE_DIR$page_dir"; echo Created \"$PAGE_DIR$page_dir\"; }
+	if [ "${page_dir##*.}" = "html" ]; then
+		# transformation for HTML files
+		sed "s/<!--DATE-->/$datestr/g" "$PAGE_FILE_DIR$page" > "$PAGE_DIR$page"
+	else
+		# copy other files
+		# TODO make this a hard link instead of a copy to save space
+		#      (need to make sure this works well enough first)
+		cp "$PAGE_FILE_DIR$page" "$PAGE_DIR$page"
+	fi
+	echo Generated \"$PAGE_DIR$page\"
+done
+
+empty_dirs=$(find "$PAGE_DIR" -type d -empty)
+for empty_dir in $empty_dirs; do
+	rmdir "$empty_dir"
+	echo Removed \"$empty_dir\"
 done
