d9b9a5f0f57a185d9df58a6e0a1b70ccc8828060
coolcuber <mrrosspgeorge@gmail.com>
2026-03-27 10:13:43 -0400

    Added backup file creation/removal

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 SOURCE="pages"
 TEMPLATE="template.html"
@@ -11,11 +12,17 @@ error() {
 mdfile="$1"
 [[ "$mdfile" =~ ^.*\.md$ ]] || error "Is \"$mdfile\" a Markdown file?"
 [ -e "$mdfile" ] || error "Couldn't find file \"$mdfile\""
+
 htmlfile="$SOURCE/$(basename "$mdfile" ".md").html"
+htmlfile_old="$htmlfile.bak"
+[ -e "$htmlfile" ] && mv -f "$htmlfile" "$htmlfile_old"
+
 pandoc --to html \
    	-V "date:$(date +"%H:%M") on $(date +"%m/%d/%Y")" \
 	--template "$TEMPLATE" \
 	"$mdfile" \
 	-o "$htmlfile"
+
 chmod a-w "$htmlfile"
+[ -e "$htmlfile_old" ] && rm -f "$htmlfile_old"
 echo "Created \"$htmlfile\""
