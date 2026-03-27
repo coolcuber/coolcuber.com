40168df6e3569052f525e54d1cd63ceaf7c99d05
coolcuber <mrrosspgeorge@gmail.com>
2026-03-27 14:48:52 -0400

    Made paths relative to top directory

--- generate.sh
+++ generate.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SOURCE="pages"
-TEMPLATE="template.html"
+scriptdir=$(dirname "$0")
+
+SOURCE="$scriptdir/pages"
+TEMPLATE="$scriptdir/template.html"
 
 error() {
 	echo "$1"
 	exit 1
 }
 
+[ "$#" = "1" ] || error "Incorrect number of arguments"
 mdfile="$1"
 [[ "$mdfile" =~ ^.*\.md$ ]] || error "Is \"$mdfile\" a Markdown file?"
 [ -e "$mdfile" ] || error "Couldn't find file \"$mdfile\""
 
-htmlfile="$SOURCE/$(basename "$mdfile" ".md").html"
+htmlfile="${mdfile%.md}.html"
 htmlfile_old="$htmlfile.bak"
 [ -e "$htmlfile" ] && mv -f "$htmlfile" "$htmlfile_old"
+# TODO check if file is in *correct* place
 
 pandoc --to html \
    	-V "date:$(date +"%H:%M") on $(date +"%m/%d/%Y")" \
